7d29901adbb3e6e19638ee3278132908fd87bbc8
Елена Антоненко <aeg-99@yandex.ru>
2022-11-04 11:33:42 +0300

    123

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -3,7 +3,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('SECRET_KEY', default='dummydjangokey')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
@@ -60,12 +59,12 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', default='django.db.backends.postgresql'),
-        'USER': os.getenv('POSTGRES_USER', default='django.db.backends.postgresql'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='django.db.backends.postgresql'),
-        'HOST': os.getenv('DB_HOST', default='django.db.backends.postgresql'),
-        'PORT': os.getenv('DB_PORT', default='django.db.backends.postgresql')
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
 
