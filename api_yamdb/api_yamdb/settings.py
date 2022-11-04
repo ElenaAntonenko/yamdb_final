6b95e505528bcf39e6dff1c72fa3477297af0e57
Елена Антоненко <aeg-99@yandex.ru>
2022-11-04 11:36:23 +0300

    1

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -3,6 +3,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+SECRET_KEY = os.getenv('SECRET_KEY', default='dummydjangokey')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
